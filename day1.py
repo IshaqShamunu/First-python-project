6c5773d05648f2764ad44f67820a897c0fe146e0
IshaqShamunu <129460725+IshaqShamunu@users.noreply.github.com>
2025-04-19 16:03:06 +0100

    Python_day_one.py

---
+++ day1.py
@@ -0,0 +1,27 @@
+firstName = "Ishaq"
+lastName = "Shamunu"
+hobby = "Anything concerning Artificial Intelligence"
+
+age = 26
+height = 1.46
+
+isstudent = False
+
+print ("what is your?") 
+print("My name is  "  + firstName)
+print()
+
+print("wow! Tell your Dad's name")
+print("His name is " + lastName)
+print()
+
+print("How old are you?")
+print("I am " + str(age) + " years old")
+print()
+
+print("Is your hobby")
+print(hobby)
+print()
+
+print("What is your height measurement")
+print("My height is " + str(height) + "cm")
